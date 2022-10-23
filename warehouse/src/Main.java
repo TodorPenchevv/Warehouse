589f2c5809ac25f4f626c03f6593c1347220b39a
TodorPenchevv <todor.k.penchev@gmail.com>
2022-10-23 21:37:24 +0300

    Hibernate
    Adding hibernate to the project

--- warehouse/src/Main.java
+++ warehouse/src/Main.java
@@ -1,3 +1,18 @@
+import ORM.UserDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
-    //App entry point
+    public static void main(String[] args) {
+        UserDetails user = new UserDetails();
+        user.setUserId(2);
+        user.setUserName("Second User");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+    }
 }
