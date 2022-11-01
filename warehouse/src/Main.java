62d916297a9af51f9c90d8316f2a189e8f12d7c7
BGSpongebob <100589893+BGSpongebob@users.noreply.github.com>
2022-11-01 18:42:21 +0200

    Fix main before new branch
    yes

--- warehouse/src/Main.java
+++ warehouse/src/Main.java
@@ -10,21 +10,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main{
+public class Main extends Application{
 
-    /*@Override
+    @Override
     public void start(Stage primaryStage) throws Exception {
         //JavaFX Setup
         Parent root = FXMLLoader.load(getClass().getResource("GUI/views/login.fxml"));
         primaryStage.setTitle("Складова програма");
         primaryStage.setScene(new Scene(root, 900, 700));
         primaryStage.show();
-    }*/
+    }
 
     public static void main(String[] args) {
         /*InsertRole.create(Roles.OPERATOR);
         InsertUser.create("momo","momo","123",1);*/
-        //launch(args);
+        launch(args);
 
     }
 }
